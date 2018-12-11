5e56d287ba033ca042b975e740b5f2f30977bf7b
Pedro Santana <pedrosantanait@gmail.com>
2018-12-10 21:00:24 -0500

    Updated model

---
+++ Events.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Events.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Events.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Events.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
